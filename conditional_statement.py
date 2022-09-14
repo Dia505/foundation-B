321402405aa9f005dd639714b4e2b7ae45e0dd7d
Dia Dewan <dia48261@gmail.com>
2022-09-14 08:15:14 +0545

    conditional_statement

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,4 +1,4 @@
-age = int(input("Please provide your age: "))
+# age = int(input("Please provide your age: "))
 
 # conditional statement
 # if block only
@@ -18,6 +18,7 @@ else:
 """
 
 # proper use of if-elif-else block
+"""
 if age<0:
     print("invalid age, please input valid age")
 elif 0<age<=10:
@@ -26,4 +27,19 @@ elif 10<age<=20:
     print("the price of the ticket is 200")
 else:
     print("the price of the ticket is 300")
-
+"""
+# ask a two integer number with user and a function should return their addition
+"""
+a = int(input("Please enter a number:"))
+b = int(input("Please enter another number:"))
+def addition(num1,num2):
+    sum = num1+num2
+    return sum
+print(f"The sum of the above numbers is {addition(a,b)}")
+"""
+brandname = str(input("Provide brand name"))
+model = str(input("Provide model name"))
+price = int(input("Provide price"))
+def info(brand_name,model_name,laptop_price):
+    return f"{brand_name} {model_name} @ Rs.{laptop_price}"
+print(info(brandname,model,price))
